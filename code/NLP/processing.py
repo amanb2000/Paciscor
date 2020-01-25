a705b58dd33decc1ac6f2ac65a50f07d0d613e4e
Divy Raval <divyr@outlook.com>
2020-01-25 18:55:06 -0500

    Create analysis methods for NLP

--- code/NLP/processing.py
+++ code/NLP/processing.py
@@ -2,7 +2,10 @@ import pandas as pd
 
 def generateProductInventory(path="src/product_dictionary.csv"):
 	"""Outputs a Dataframe structure, a column vector with names of possible products."""
-	return pd.read_csv(path)
+	df = pd.read_csv(path)
+	for i in range(df["product_name"].count()):
+		df["product_name"][i] = df["product_name"][i].lower()
+	return df
 
 
 def generateUnitsInventory(path="src/units_dictionary.csv"):
@@ -11,3 +14,4 @@ def generateUnitsInventory(path="src/units_dictionary.csv"):
 
 if __name__ == "__main__":
 	print(generateProductInventory())
+	print(generateUnitsInventory())
