2dbe40831f862119ad183f91541e1a97d7f4d195
Adam Carnaffan <acarnaffan@hotmail.com>
2020-01-26 04:03:45 -0500

    Sending Segmentation to Divy

--- code/Segmentation/4_layer_mask.py
+++ code/Segmentation/4_layer_mask.py
@@ -2,6 +2,7 @@ import cv2
 import numpy as np
 import matplotlib.pyplot as plt
 from matplotlib import cm
+import matplotlib.patches as patches
 import os.path as pt
 
 COLORS = [[149, 255, 192], [58, 50, 31], [18, 205, 41], [55, 29, 86], [13, 171, 58], [87, 125, 121], [146, 178, 99], [245, 245, 17], [124, 218, 156],
@@ -12,8 +13,9 @@ COLORS = [[149, 255, 192], [58, 50, 31], [18, 205, 41], [55, 29, 86], [13, 171, 
 ACCURACY = 0.1
 MAX_ITERATIONS = 100
 MAX_TRIALS = 20
-LOWEST_CLUSTERS = 10
-HIGHEST_CLUSTERS = 20
+LOWEST_CLUSTERS = 3
+HIGHEST_CLUSTERS = 3
+TARGET_COMPAT = 27000000
 
 def read_image(target):
     # FIX DIS
@@ -41,6 +43,8 @@ pixel_values = np.float32(pixel_values)
 
 criteria = (cv2.TERM_CRITERIA_EPS + cv2.TERM_CRITERIA_MAX_ITER, MAX_ITERATIONS, ACCURACY)
 
+currentBest = [0, None, None]
+
 for clusters in range(LOWEST_CLUSTERS, HIGHEST_CLUSTERS+1, 1):
     compat, labels, (centers) = cv2.kmeans(pixel_values, clusters, None, criteria, MAX_TRIALS, cv2.KMEANS_RANDOM_CENTERS)
 
@@ -49,18 +53,35 @@ for clusters in range(LOWEST_CLUSTERS, HIGHEST_CLUSTERS+1, 1):
 
     labels = labels.flatten()
 
-    pts = centers[labels.flatten()]
-
+    
     # Prep plot data
     final = np.full((image[2][0], image[2][1], 3), 255)
+    truthImg = np.full((image[2][0], image[2][1]), -1)
 
     for pt, val in zip(image[1], labels.flatten()):
         final[pt[0], pt[1]] = COLORS[val]
+        truthImg[pt[0], pt[1]] = val
 
     # Plot
+    plt.axes()
     plt.imshow(final)
+
+    # Add rectangles
+    rect = []
+    for lbl in range(0, clusters, 1):
+        locs = np.where(truthImg==lbl)
+        x_min, x_max, y_min, y_max = np.amin(locs[1]), np.amax(locs[1]), np.amin(locs[0]), np.amax(locs[0])
+        print(x_min, x_max, y_min, y_max)
+        # rect += [patches.Rectangle((x_min,y_max), x_max - x_min, y_max - y_min, linewidth=2, edgecolor=COLORS[lbl], facecolor='none')]
+        # plt.gca().add_patch(rect[lbl])
+        # plt.show()
+
+    # Save
     plt.savefig('new_plot_K_{}_compat_{}.png'.format(clusters, int(compat)))
 
-# for pts in centers:
-#     plt.plot(pts[0], pts[1], 'ro')
-# plt.savefig('testing.png')
+    # Compare for best clustering
+    if currentBest[1] is None or abs(TARGET_COMPAT - compat) < currentBest[1]:
+        currentBest = [clusters, abs(TARGET_COMPAT - compat), (labels, centers)]
+
+print("It is concluded that the best clustering is {}".format(currentBest[0]))
+
