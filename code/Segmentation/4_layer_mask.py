1804553f92fea2413e2619f452889a840b573d7e
Adam Carnaffan <acarnaffan@hotmail.com>
2020-01-26 00:15:46 -0500

    Formalizing appraoch

--- code/Segmentation/4_layer_mask.py
+++ code/Segmentation/4_layer_mask.py
@@ -4,6 +4,17 @@ import matplotlib.pyplot as plt
 from matplotlib import cm
 import os.path as pt
 
+COLORS = [[149, 255, 192], [58, 50, 31], [18, 205, 41], [55, 29, 86], [13, 171, 58], [87, 125, 121], [146, 178, 99], [245, 245, 17], [124, 218, 156],
+            [14, 182, 155], [183, 207, 161], [38, 209, 25], [22, 185, 127], [250, 116, 161], [167, 253, 28], [110, 224, 160], [175, 16, 146],
+            [101, 62, 167], [75, 247, 89], [248, 66, 241]]
+
+# CONTROLS
+ACCURACY = 0.1
+MAX_ITERATIONS = 100
+MAX_TRIALS = 20
+LOWEST_CLUSTERS = 10
+HIGHEST_CLUSTERS = 20
+
 def read_image(target):
     # FIX DIS
     path = '../../../src/flyers/{}'.format(target)
@@ -21,50 +32,34 @@ def read_image(target):
                 mappedImg.append([i, j])
     return invImg, np.array(mappedImg), (height, width)
 
+
+
 image = read_image('week_1_page_1.jpg')
-# Invert
 
 pixel_values = image[1].reshape((-1, 2))
 pixel_values = np.float32(pixel_values)
-# print(pixel_values.shape)
 
-criteria = (cv2.TERM_CRITERIA_EPS + cv2.TERM_CRITERIA_MAX_ITER, 100, 0.2)
-k = 18
+criteria = (cv2.TERM_CRITERIA_EPS + cv2.TERM_CRITERIA_MAX_ITER, MAX_ITERATIONS, ACCURACY)
 
-_, labels, (centers) = cv2.kmeans(pixel_values, k, None, criteria, 10, cv2.KMEANS_RANDOM_CENTERS)
+for clusters in range(LOWEST_CLUSTERS, HIGHEST_CLUSTERS+1, 1):
+    compat, labels, (centers) = cv2.kmeans(pixel_values, clusters, None, criteria, MAX_TRIALS, cv2.KMEANS_RANDOM_CENTERS)
 
-# convert back to 8 bit values
-# print(centers)
+    # Normalize results
+    centers = np.int16(centers)
 
-centers = np.int16(centers)
+    labels = labels.flatten()
 
-print(centers)
+    pts = centers[labels.flatten()]
 
-# flatten the labels array
-# print(labels)
+    # Prep plot data
+    final = np.full((image[2][0], image[2][1], 3), 255)
 
-colors = [[149, 255, 192], [58, 50, 31], [18, 205, 41], [55, 29, 86], [13, 171, 58], [87, 125, 121], [146, 178, 99], [245, 245, 17], [124, 218, 156],
-            [14, 182, 155], [183, 207, 161], [38, 209, 25], [22, 185, 127], [250, 116, 161], [167, 253, 28], [110, 224, 160], [175, 16, 146],
-            [101, 62, 167], [75, 247, 89], [248, 66, 241]]
+    for pt, val in zip(image[1], labels.flatten()):
+        final[pt[0], pt[1]] = colors[val]
 
-labels = labels.flatten()
-
-pts = centers[labels.flatten()]
-
-final = np.full((image[2][0], image[2][1], 3), 255)
-
-for pt, val in zip(image[1], labels.flatten()):
-    final[pt[0], pt[1]] = colors[val]
-
-print(final)
-# print(labels)
-
-# segmented_image = centers[labels.flatten()]
-
-# segmented_image = segmented_image.reshape(image.shape)
-# # show the image
-plt.imshow(final)
-plt.savefig('testing.png')
+    # Plot
+    plt.imshow(final)
+    plt.savefig('new_plot_K_{}_compat_{}.png'.format(clusters, int(compat)))
 
 # for pts in centers:
 #     plt.plot(pts[0], pts[1], 'ro')
